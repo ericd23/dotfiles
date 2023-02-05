19a4cac088fc459a4c1ae8adfe28b0e3827d72ca
ericd <ed@mailbox.org>
2023-02-05 22:36:36 +0800

    bye doom

--- config/doom/profiles.el
+++
@@ -1,9 +0,0 @@
-((doom
-  (user-emacs-directory . "~/.config/emacs")
-  (doom-user-dir . "~/.config/doom")
-  (doom-cache-dir :path doom-user-dir ".local/cache")
-  (doom-data-dir  :path doom-user-dir ".local/data")
-  (doom-state-dir :path doom-user-dir ".local/state"))
-
- (vanilla
-  (user-emacs-directory . "~/.config/emacs.vanilla")))
