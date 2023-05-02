23b37a9ecf5a2a1cb78df3e3f97f37135dedc9fb
ericd <ed@mailbox.org>
2023-05-02 15:18:18 +0800

    remove tmux aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,7 @@ fish_vi_key_bindings
 
 alias l="ls -alFh"
 alias lt="ls -alFht"
+
 alias ga="git add"
 alias gd="git diff"
 alias gst="git status"
@@ -14,6 +15,7 @@ alias glg="git log --graph --pretty=format:'%h/%an - %s'"
 alias gus="git restore --staged"
 alias gdog="git log --all --decorate --oneline --graph"
 alias gb="git branch"
+
 alias ..="cd .."
 alias bs="sudo powertop --auto-tune"
 alias grl="grep --recursive --line-number --binary-files=without-match --exclude-dir=node_modules --exclude-dir=.git"
@@ -24,9 +26,7 @@ alias pms="pacman -Ss"
 alias pmf="pacman -Fl"
 alias s="sudo su"
 alias bm=bashmount
-alias t="tmux new"
-alias tl="tmux list-sessions"
-alias tk="tmux kill-server"
+
 alias red="sudo sh -c 'rmmod psmouse; modprobe psmouse proto=imps'"
 alias tap="xinput --set-prop 'Synaptics TM3203-003' 'libinput Tapping Enabled' 1"
 
