9b9b76dc9b3b717ad0030f4aaac19c620658b00e
ericd <ed@mailbox.org>
2023-05-02 15:18:37 +0800

    vis: more complete config

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -1,17 +1,28 @@
--- load standard vis module, providing parts of the Lua API
 require('vis')
 
+local plug = require('plugins/vis-plug')
+
 vis.events.subscribe(vis.events.INIT, function()
-	-- global configuration options
+    -- global configuration options
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	-- per window configuration options
-	vis:command('set tw 2')
-	vis:command('set ai')
-	vis:command('set nu')
-	vis:command('set cul')
-	vis:command('set cc 80')
-	vis:command('set theme default-256')
-	vis:command('set ic')
+    -- per window configuration options
+    vis:command('set tw 4')
+    vis:command('set ai')
+    vis:command('set et')
+    vis:command('set cul')
+    vis:command('set cc 80')
+    vis:command('set ic')
 end)
+
+local plugins = {
+    { 'lutobler/vis-commentary' },
+    -- { 'git.sr.ht/~emg/vis-cscope' },
+    { 'repo.or.cz/vis-pairs.git' },
+    { 'erf/vis-sneak' },
+    { 'repo.or.cz/vis-surround.git' },
+    { 'przmv/base16-vis', theme = true, file = 'themes/base16-ashes' }
+}
+
+plug.init(plugins, true)
